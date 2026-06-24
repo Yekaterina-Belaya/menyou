74a6223d3ad3ec59f768522b4ff8fbd3831ea3b8
Yekaterina <85305642+Yekaterina-Belaya@users.noreply.github.com>
2026-06-24 11:34:53 +0400

    i18n, playwright, karma configurations, package.json and angular.json changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, ChangeDetectionStrategy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
+  standalone: true,
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  changeDetection: ChangeDetectionStrategy.Eager,
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('menyou-app');
